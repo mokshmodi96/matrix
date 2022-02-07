75dd56364524e6820e0f403fe004f77d6630f7d1
mokshmodi <mokshmodi96>
2022-02-07 20:24:19 +0530

    ✨ Matrix Rainfall Effect using Skia

--- App.tsx
+++ App.tsx
@@ -1,39 +1,8 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Canvas, Circle, Group} from '@shopify/react-native-skia';
+import {Matrix} from './src/components/Matrix';
 
 const App = () => {
-  const width = 256;
-  const height = 256;
-  const r = 215;
-  return (
-    <Canvas style={{flex: 1}}>
-      <Group blendMode="multiply">
-        <Circle cx={r} cy={r} r={r} color="cyan" />
-        <Circle cx={width - r} cy={r} r={r} color="magenta" />
-        <Circle cx={width / 2} cy={height - r} r={r} color="yellow" />
-      </Group>
-    </Canvas>
-  );
+  return <Matrix />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ readme.md
@@ -0,0 +1,7 @@
+<h1 align="center">Matrix Rainfall Effect in React Native using Skia</h1>
+
+Skia is new UI library backed by Shopify. You can create Animations really easily using this library.
+
+Inspiration from William Candillon's Video
+
+https://youtu.be/49QR0wUDMG0
